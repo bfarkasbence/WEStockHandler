dbf69f1493e8f5d1acff726169459f33785f2cc2
Farkas Bence <b@LAPTOP-TH590FDE>
2020-09-02 14:24:18 +0200

    created cart item model

---
+++ WEStockHandler/Models/CartItemModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEStockHandler.Models
+{
+    public class CartItemModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int productPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
