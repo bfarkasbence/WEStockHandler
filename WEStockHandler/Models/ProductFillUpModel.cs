8ff82bd2e9b197bde3319c6313bd9a5adc40d6bb
Farkas Bence <b@LAPTOP-TH590FDE>
2020-09-23 15:56:15 +0200

    add new productFillUp model

---
+++ WEStockHandler/Models/ProductFillUpModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEStockHandler.Models
+{
+    public class ProductFillUpModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ProductCode { get; set; }
+        public int CartonCode { get; set; }
+        public int RequiedQuantity { get; set; }
+        public int SendQuantity { get; set; }
+
+    }
+}
