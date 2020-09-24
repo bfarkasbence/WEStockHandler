36e406d55b330b97b44fe33753c42bd9784124cf
Farkas Bence <b@LAPTOP-TH590FDE>
2020-09-24 18:30:04 +0200

    add new sent product model to the models

---
+++ WEStockHandler/Models/SentProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEStockHandler.Models
+{
+    public class SentProductModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductCode { get; set; }
+        public int SentQuantity { get; set; }
+        public int RecievedQuantity { get; set; }
+    }
+}
